3a93f2c463d5dda309cf039fe75ff2a61a84c975
Marcin Branski <6998565+3h4x@users.noreply.github.com>
2026-05-11 22:11:08 +0200

    fix(project): validate tabs and submit form

--- app/project/[name]/page.tsx
+++ app/project/[name]/page.tsx
@@ -1,18 +1,5 @@
-'use client'
-
-import { useProjects } from '@/components/ProjectsProvider'
-import { ProjectDetailPage } from '@/components/ProjectDetailPage'
-import { LoadingState } from '@/components/LoadingState'
+import { ProjectPageShell } from '@/components/project-detail/ProjectPageShell'
 
 export default function ProjectPage() {
-  const { tasks, loading, fleet, loadProjects } = useProjects()
-
-  if (loading && tasks.length === 0) return <LoadingState />
-
-  return (
-    <ProjectDetailPage
-      fleet={fleet}
-      onRefresh={loadProjects}
-    />
-  )
+  return <ProjectPageShell />
 }
